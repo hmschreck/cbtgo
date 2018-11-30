f4083aea6cf82fa46fec3dbf47a6cdb74bfc4f93
Harold Schreckengost <harold@haroldmschreckengost.com>
2018-11-29 18:23:36 -0600

    Fixed Testing

--- api_test.go
+++ api_test.go
@@ -7,14 +7,14 @@ const TestKey = "pass"
 const TestDownloadPath = "./"
 func TestSetUpAuth(t *testing.T) {
 	SetUpAuth(TestUser, TestKey)
-	if !(username == TestUser && authkey == TestKey) {
+	if !(Username == TestUser && Authkey == TestKey) {
 		t.Errorf("Could not set authentication")
 	}
 }
 
 func TestSetDownload(t *testing.T) {
 	SetDownload(TestDownloadPath)
-	if downloadPath != TestDownloadPath {
+	if DownloadPath != TestDownloadPath {
 		t.Errorf("Could not set downloadpath")
 	}
 }
